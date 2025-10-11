385032aac81853a85d09d7980a3477419361d413
Doez663 <110844447+Doez663@users.noreply.github.com>
2025-10-11 23:29:14 +0800

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -1,21 +1,21 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
-        username: 'nxtlnk.vercel.app',
+        name: 'Sisi Lee',
+        username: '@sisileex',
         url: 'https://nxtlnk.vercel.app',
         titleImg: true,
         avatar: '/avatar.png',
-        nftAvatar: true,
+        nftAvatar: false,
         description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
         descShow: true,
         subdesc: 'Use this template and deploy on vercel for free',
         subdescShow: true,
         newProductUrl: 'https://3dicons.co',
         newProduct: true,
-        author: 'realvjy',
-        authorURL: 'https://twitter.com/realvjy',
-        footerText: 'made by'
+        // author: 'realvjy',
+        // authorURL: 'https://twitter.com/realvjy',
+        // footerText: 'made by'
     },
 ];
 
