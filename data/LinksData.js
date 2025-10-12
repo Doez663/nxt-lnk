5025a3c8adb78dfb8270b09c9cddc2e8c4bcd016
Doez663 <110844447+Doez663@users.noreply.github.com>
2025-10-12 11:12:35 +0800

    Update LinksData.js

--- data/LinksData.js
+++ data/LinksData.js
@@ -19,7 +19,7 @@ const webLinks = [
         title: 'Threads',
         url: 'https://www.threads.com/@sisi.leex',
         type: 'social',
-        icon: '/threads.png',
+        icon: '/threads.svg',
         on: true
     },
     {
